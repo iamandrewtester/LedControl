9662b7a32d24cddb2bf03cb8112675aa72b26b7f
Andrew Tester <atester42@hotmail.com>
2020-10-11 18:34:37 -0700

    LedControl Initial commit.

---
+++ LedControl/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace LedControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Red = new System.Windows.Forms.HScrollBar();
+            this.Green = new System.Windows.Forms.HScrollBar();
+            this.Blue = new System.Windows.Forms.HScrollBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Red
+            // 
+            this.Red.Location = new System.Drawing.Point(29, 76);
+            this.Red.Name = "Red";
+            this.Red.Size = new System.Drawing.Size(182, 24);
+            this.Red.TabIndex = 1;
+            this.Red.Scroll += new System.Windows.Forms.ScrollEventHandler(this.Red_Scroll);
+            // 
+            // Green
+            // 
+            this.Green.Location = new System.Drawing.Point(29, 120);
+            this.Green.Name = "Green";
+            this.Green.Size = new System.Drawing.Size(182, 27);
+            this.Green.TabIndex = 1;
+            this.Green.Scroll += new System.Windows.Forms.ScrollEventHandler(this.Green_Scroll);
+            // 
+            // Blue
+            // 
+            this.Blue.Location = new System.Drawing.Point(29, 172);
+            this.Blue.Name = "Blue";
+            this.Blue.Size = new System.Drawing.Size(182, 24);
+            this.Blue.TabIndex = 1;
+            this.Blue.Scroll += new System.Windows.Forms.ScrollEventHandler(this.Blue_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(228, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(228, 120);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(228, 172);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(29, 230);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Red";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Green";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Blue";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 262);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Blue);
+            this.Controls.Add(this.Green);
+            this.Controls.Add(this.Red);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.HScrollBar Red;
+        private System.Windows.Forms.HScrollBar Green;
+        private System.Windows.Forms.HScrollBar Blue;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LedControl/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace LedControl
+{
+    public partial class Form1 : Form
+    {
+
+        private TcpClient client = new TcpClient();
+        private int rVal, gVal, bVal;
+        private byte[] arr = new byte[16];
+        private NetworkStream stream;
+        private String message;
+        public Form1()
+        {
+            InitializeComponent();
+            rVal = bVal = gVal = 0;
+            textBox1.Text = rVal.ToString();
+            textBox2.Text = gVal.ToString();
+            textBox3.Text = bVal.ToString();
+            if (tcpConnect()==0)
+                textBox4.Text = "Connected!";
+        }
+
+        private int tcpConnect()
+        {
+
+            try
+            {
+
+                client.Connect("192.168.1.74", 7);
+                stream = client.GetStream();
+
+            }
+            catch(Exception e)
+            {
+                textBox4.Text = "Connection failed";
+                return 1;
+            }
+            return 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Reset Control
+            bVal = Blue.Value;
+            message = "S0";
+            //arr = System.Text.Encoding.ASCII.GetBytes(message);
+            arr[0] = (byte)'S';
+            arr[1] = 0;
+            stream.Write(arr, 0, 2);
+            message = "";
+        }
+
+        private void Red_Scroll(object sender, ScrollEventArgs e)
+        {
+            //Red Control
+            rVal = Red.Value;
+            textBox1.Text = rVal.ToString();
+            //message="R"+ rVal.ToString();
+            //arr = System.Text.Encoding.ASCII.GetBytes(message);
+            arr[0] = (byte)'R';
+            arr[1] = (byte)rVal;
+            stream.Write(arr, 0, 2);
+            //message = "";
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Green_Scroll(object sender, ScrollEventArgs e)
+        {
+            //Green Control
+            gVal = Green.Value;
+            textBox2.Text = gVal.ToString();
+            //message = "G" + gVal.ToString();
+            //arr = System.Text.Encoding.ASCII.GetBytes(message);
+            arr[0] = (byte)'G';
+            arr[1] = (byte)gVal;
+            stream.Write(arr, 0, 2);
+            //message = "";
+        }
+
+        private void Blue_Scroll(object sender, ScrollEventArgs e)
+        {
+            //Blue Control
+            bVal = Blue.Value;
+            textBox3.Text = bVal.ToString();
+            //message = "B" + bVal.ToString();
+            //arr = System.Text.Encoding.ASCII.GetBytes(message);
+            arr[0] = (byte)'B';
+            arr[1] = (byte)bVal;
+            stream.Write(arr, 0, arr.Length);
+            //message = "";
+        }
+    }
+}
